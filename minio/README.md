daf4ab6f69a212b0c90d2e4718868042a0ec07e4
문호성 <mhs@laonpeople.com>
2025-03-07 15:33:01 +0900

    minio 추가

---
+++ minio/README.md
@@ -0,0 +1,13 @@
+# README
+
+- 헬름 차트로 minio 설치
+- 
+
+### Helm Chart Document
+
+- 🔗 Link : (https://artifacthub.io/packages/helm/bitnami/minio)
+
+### Set-up and Install
+
+1. values.yaml 파일에 필요한 키/값 지정
+2. cmd.sh 실행
---
+++ minio/cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 헬름 차트 추가
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+# # 헬름 차트 설치
+helm install minio bitnami/minio \
+    -n minio --create-namespace \
+    -f values.yaml \
+    --version 15.0.6
