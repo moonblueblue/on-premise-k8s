cd5261a4c9da84efb286bdbbddf13ff5a2ddfae0
mhs <mhs@laonpeople.com>
2025-03-08 11:19:11 +0900

    argocd 추가

---
+++ argocd/README.md
@@ -0,0 +1,13 @@
+# README
+
+- 헬름 차트로 argocd 설치
+- 쿠버네티스 CD 배포 파이프라인
+
+### Helm Chart Document
+
+- 🔗 Link : (https://artifacthub.io/packages/helm/argo/argo-cd)
+
+### Set-up and Install
+
+1. values.yaml 파일에 필요한 키/값 지정
+2. cmd.sh 실행
---
+++ argocd/cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 헬름 차트 추가
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+# # 헬름 차트 설치
+helm install argocd argo/argo-cd \
+    -n argocd --create-namespace \
+    -f values.yaml \
+    --version 7.8.8
