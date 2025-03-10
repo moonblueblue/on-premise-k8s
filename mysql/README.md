a760b45c14151db9dd27a075493891c80396109a
문호성 <mhs@laonpeople.com>
2025-03-10 16:49:30 +0900

    mysql 추가

---
+++ mysql/README.md
@@ -0,0 +1,13 @@
+# README
+
+- 헬름 차트로 mysql 설치
+- RDMBS 데이터베이스
+
+### Helm Chart Document
+
+- 🔗 Link : (https://artifacthub.io/packages/helm/bitnami/mysql)
+
+### Set-up and Install
+
+1. values.yaml 파일에 필요한 키/값 지정
+2. cmd.sh 실행
---
+++ mysql/cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 헬름 차트 추가
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+# # 헬름 차트 설치
+helm install mysql bitnami/mysql \
+    -n mysql --create-namespace \
+    -f values.yaml \
+    --version 12.3.1
