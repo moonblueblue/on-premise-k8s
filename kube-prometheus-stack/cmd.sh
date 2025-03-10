5b156f609723798052db9157d21898f4523d90a0
mhs <mhs@laonpeople.com>
2025-03-11 06:40:10 +0900

    kube-prometheus-stack 리소스 재조정

--- kube-prometheus-stack/cmd.sh
+++ kube-prometheus-stack/cmd.sh
@@ -5,7 +5,7 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 # # 헬름 차트 설치
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
+helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
     -n kube-prometheus-stack --create-namespace \
     -f values.yaml \
     --version 69.8.0
