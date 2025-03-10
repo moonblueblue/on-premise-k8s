6c1b962e5e14856d93bfb17cba7cd9e81e2c1348
mhs <mhs@laonpeople.com>
2025-03-10 23:13:13 +0900

    리소스 조정 초기화

--- argocd/cmd.sh
+++ argocd/cmd.sh
@@ -5,7 +5,7 @@ helm repo add argo https://argoproj.github.io/argo-helm
 helm repo update
 
 # # 헬름 차트 설치
-helm install argocd argo/argo-cd \
+helm upgrade --install argocd argo/argo-cd \
     -n argocd --create-namespace \
     -f values.yaml \
     --version 7.8.8
