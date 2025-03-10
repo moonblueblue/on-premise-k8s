a02ca2e17fa184f91add261d9bdc8bd73e92794b
mhs <mhs@laonpeople.com>
2025-03-10 23:04:04 +0900

    harbor 리소스 재조정

--- harbor/cmd.sh
+++ harbor/cmd.sh
@@ -5,7 +5,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # # 헬름 차트 설치
-helm install harbor bitnami/harbor \
+helm upgrade --install harbor bitnami/harbor \
     -n harbor --create-namespace \
     -f values.yaml \
     --version 24.3.4
