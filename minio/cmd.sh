7637b8ce8c7316a73e1424c78f254447bf9f50e8
moonblueblue <devbluemoons@gmail.com>
2025-04-29 21:55:53 +0900

    minio 리소스 설정 및 볼륨 권한 수정

--- minio/cmd.sh
+++ minio/cmd.sh
@@ -5,7 +5,7 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # # 헬름 차트 설치
-helm install minio bitnami/minio \
+helm upgrade --install minio bitnami/minio \
     -n minio --create-namespace \
     -f values.yaml \
     --version 15.0.6
